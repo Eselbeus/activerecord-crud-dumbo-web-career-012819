834961b85416e4128d0a086324771f004e00d756
eselbeus <eselbeus@gmail.com>
2019-02-08 19:21:58 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -65,8 +65,6 @@ def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
   movs = Movie.where(self.release_date > 2003)
-  movs.sort.reverse
-  movs.save
 end
 
 def can_be_found_updated_and_saved
